368ac671caac3c17cd548c4aad4ec435e29958ca
Deepesh Pathak <deepshpathak@gmail.com>
2020-12-23 18:41:42 +0530

    operator: add metrics list command for cilium-operator
    Signed-off-by: Deepesh Pathak <deepshpathak@gmail.com>

---
+++ operator/cmd/root.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/cilium/cilium/operator/option"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var (
+	operatorAddr string
+
+	log     = logrus.New()
+	verbose = false
+)
+
+// Populate options required by cilium-operator command line only.
+func Populate() {
+	operatorAddr = viper.GetString(option.OperatorAPIServeAddr)
+}
